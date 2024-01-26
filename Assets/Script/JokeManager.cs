a38b7ad726f742232ee8739d0ff775675eddea41
Ivan Ho <38537881+hhkit@users.noreply.github.com>
2024-01-27 00:10:55 +0800

    rejection logic

--- Assets/Script/JokeManager.cs
+++ Assets/Script/JokeManager.cs
@@ -1,8 +1,26 @@
 
 
+using System;
+using System.Linq;
 using UnityEngine;
 
 public class JokeManager : MonoBehaviour
 {
-    public JokesDataSO jokes;
+    public JokesDataSO jokeData;
+    public GameObject jesterPrefab;
+
+    private Joke[] funnyJokes;
+    private Joke[] unfunnyJokes;
+
+    public void Awake()
+    {
+        funnyJokes = jokeData.Jokes.Where(a => a.IsFunny).ToArray();
+        unfunnyJokes = jokeData.Jokes.Where(a => a.IsLame).ToArray();
+    }
+
+    public Joke[] CreateJokeQueue(JesterConfig config)
+    {
+        // wtf why is my brain working not
+        return new Joke[0];
+    }
 }

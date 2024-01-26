1ce945127ed7537f1b7518c26e5e6fd0cb5d5bd5
Nicholas Kang Zhi Hao <nicholaskang95@gmail.com>
2024-01-26 22:26:24 +0800

    import tmp

---
+++ Assets/Scripts/Jester.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jester : MonoBehaviour
+{
+    public Joke m_Joke;
+    public JokerRace m_JokerRace;
+    void Start()
+    {
+        
+    }
+	
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Joke.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Joke 
+{
+    public JokeType jokeType;
+    public JokeLevel jokeLevel;
+    public string m_JokeString;
+}

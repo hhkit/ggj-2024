3a92842a220b47cb862fb0eb8cca2f69a5f143a0
Ivan Ho <38537881+hhkit@users.noreply.github.com>
2024-01-26 22:21:18 +0800

    game scene

---
+++ Assets/Script/DayManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using YamlDotNet.Serialization;
+
+public class DayManager : MonoBehaviour
+{
+    public TextAsset[] days;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
